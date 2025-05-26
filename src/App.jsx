a58dd352db0f5b42765cc4a37b70d046767cfee4
Tatva Agarwal <tatva.agarwal.2006@gmail.com>
2025-05-26 23:56:43 +0530

    Add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,48 @@
 import { useEffect, useState } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./components/loginPage";
-import pb from "./services/pocketbase";
 import HomePage from "./components/homePage";
+import pb from "./services/pocketbase";
 
 function App() {
-  // console.log(pb.authStore.isValid);
   const [loggedIn, setLoggedIn] = useState(pb.authStore.isValid);
 
   useEffect(() => {
-    // Update loggedIn when authStore changes
     return pb.authStore.onChange(() => {
       setLoggedIn(pb.authStore.isValid);
-      
     });
   }, []);
 
-  return loggedIn ? <HomePage /> : <LoginPage />;
+  // Protected Route Wrapper
+  const PrivateRoute = ({ children }) => {
+    return loggedIn ? children : <Navigate to="/login" replace />;
+  };
+
+  const PublicRoute = ({ children }) => {
+    return loggedIn ? <Navigate to="/" replace /> : children;
+  };
+
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <PrivateRoute>
+            <HomePage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/login"
+        element={
+          <PublicRoute>
+            <LoginPage />
+          </PublicRoute>
+        }
+      />
+      <Route path="*" element={<Navigate to={loggedIn ? "/" : "/login"} />} />
+    </Routes>
+  );
 }
 
 export default App;
