3081280ab393dc4ad8669d45c99254afb4af9513
Tatva Agarwal <tatva.agarwal.2006@gmail.com>
2025-06-09 19:38:22 +0530

    Fixed username trimming

--- src/components/ChangeNameForm.jsx
+++ src/components/ChangeNameForm.jsx
@@ -9,7 +9,7 @@ export default function ChangeNameCard({onNameChange = null}){
     async function handleSubmit() {
         try {
           const updatedRecord = await pb.collection("users").update(pb.authStore.record.id, {
-            name: currentName,
+            name: currentName.trim(),
           });
     
           await pb.collection("users").authRefresh();
@@ -25,14 +25,14 @@ export default function ChangeNameCard({onNameChange = null}){
     return (
         <fieldset className="fieldset bg-base-200 rounded-box w-full p-1">
             <label className="input">
-              <div className={`status ${pb.authStore.record.name === currentName ? "status-warning animate-pulse" : currentName.length < 4 ? "status-error animate-bounce" : "status-success animate-none" }`} />
-              <input type="text" placeholder="John Doe" value={currentName} onChange={(event) => {setCurrentName(event.target.value.trim())}}/>
+              <div className={`status ${pb.authStore.record.name === currentName.trim() ? "status-warning animate-pulse" : currentName.trim().length < 4 ? "status-error animate-bounce" : "status-success animate-none" }`} />
+              <input type="text" placeholder="John Doe" value={currentName} onChange={(event) => {setCurrentName(event.target.value)}}/>
             </label>
             <p className="label max-w-xs text-wrap">Full name is shown if no spaces present, else only first part is shown on homepage.</p>
 
             <div className="join">
                 <button className="btn join-item btn-soft mt-4" onClick={() => setCurrentName(pb.authStore.record.name)}>Cancel</button>
-                <button className="btn join-item btn-info mt-4" onClick={handleSubmit} disabled={pb.authStore.record.name === currentName || currentName.length < 4}>Save</button>
+                <button className="btn join-item btn-info mt-4" onClick={handleSubmit} disabled={pb.authStore.record.name === currentName.trim() || currentName.trim().length < 4}>Save</button>
             </div>
         </fieldset>
     );
