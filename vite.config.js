e50852a8c3d3555385b907175b7d4e375cd18759
Tatva Agarwal <tatva.agarwal.2006@gmail.com>
2025-06-09 23:22:34 +0530

    Enable compressions

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,26 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
+import compression from 'vite-plugin-compression';
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    compression({ algorithm: 'gzip', ext: '.gz' }),
+    compression({ algorithm: 'brotliCompress', ext: '.br' })
+    // visualizer({
+    //   filename: 'dist/stats.html',
+    //   open: true, // Automatically open in browser after build
+    //   gzipSize: true,
+    //   brotliSize: true
+    // })
   ],
   resolve: {
     extensions: ['.js', '.jsx', '.json']
+  },
+  build: {
+    minify: 'terser',
+    cssCodeSplit: true
   }
-  
 });
